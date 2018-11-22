d87d12707404a239106da0442cc565648f8bc7bb
jjone <jiwon.j306@gmail.com>
2018-11-22 17:05:08 +0900

    cleaning

--- cosmetic.py
+++ cosmetic.py
@@ -1,11 +1,11 @@
 ####################### 1. Web Scraping
 import time
-import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+
 
 chrome_path = "C:\\Users\jjone\Downloads\chromedriver"
 
@@ -73,13 +73,11 @@ df2 = pd.DataFrame(columns=['brand', 'name', 'price', 'rank', 'skin_type', 'ingr
 df = pd.concat([df, df2], axis = 1)
 
 # step 2
-import re
-
 for i in range(len(df)+1):
     url = df.URL[i]
     driver.get(url)
     time.sleep(5)
-    
+
     xpath = '/html/body/div[5]/div/div/div[1]/div/div/button'
     btn = driver.find_element_by_xpath(xpath)
     btn.click()
@@ -88,7 +86,7 @@ for i in range(len(df)+1):
     df.brand[i] = driver.find_element_by_class_name('css-avdj50').text
     df.name[i] = driver.find_element_by_class_name('css-r4ddnb ').text
     df.price[i] = driver.find_element_by_class_name('css-n8yjg7 ').text
-    
+
     browser = scrollDown(driver, 1)
     time.sleep(5)
     browser = scrollDown(driver, 1)
@@ -104,43 +102,146 @@ for i in range(len(df)+1):
     btn = driver.find_element_by_xpath(xpath)
     btn.click()
 
-    try: 
+    try:
         df.ingredients[i] = driver.find_element_by_xpath('//*[@id="tabpanel2"]/div').text
     except NoSuchElementException:
         df.ingredients[i] = 'No Info'
-        
+
     # rank
     try:
-        rank = driver.find_element_by_class_name('css-ffj77u').text  
-        rank = re.match('\d.\d', rank).group() 
+        rank = driver.find_element_by_class_name('css-ffj77u').text
+        rank = re.match('\d.\d', rank).group()
         df['rank'][i] = str(rank)
-        
+
     except NoSuchElementException:
         df['rank'][i] = 0
-    
+
     print(i)    # just for verbose
 
 
 df.to_csv('cosmetic.csv', encoding = 'utf-8-sig', index = False)
 
-cosmetic = pd.read_csv('cosmetic.csv')
-
 ####################### 2. Cleaning data
-from nltk.tokenize import word_tokenize
-tokens = word_tokenize()
+import pandas as pd
+import numpy as np
+import re
 
 
+cosm = pd.read_csv('cosmetic.csv')
+cosm.info()
+
+cosm = cosm.loc[pd.notnull(cosm['ingredients'])]
+cosm.info()
+
+# label
+cosm.Label[cosm['Label'] == 'moisturizing-cream-oils-mists'] = str('moisturizer')
+cosm.Label[cosm['Label'] == 'eye-treatment-dark-circle-treatment'] = str('eye-cream')
+cosm.Label[cosm['Label'] == 'sunscreen-sun-protection'] = str('sun-protection')
+
+# name -> duplicated item
+df_2 = cosm['name'].drop_duplicates()
+cosm = cosm.loc[df_2.index, :].reset_index()
+
+# URL
+cosm.drop(['URL', 'index'], axis = 1, inplace = True)
+
+# price
+pattern = re.compile(r"(\d+).\d+")
+for i in range(len(cosm)):
+    cosm['price'][i] = re.findall(pattern, cosm['price'][i])[0]
+
+cosm['price'] = pd.to_numeric(cosm['price'])
+
+# rank
+cosm['rank'].fillna(0, inplace = True)
+cosm.info()
+
+# skin_type
+pattern = re.compile(r"([a-zA-Z]+)\\n")
+for i in range(len(cosm)):
+    cosm['skin_type'][i] = re.findall(pattern, cosm['skin_type'][i])
+
+## list column dummies
+df_2 = cosm['skin_type'].str.join('|').str.get_dummies().add_prefix('type_')
+cosm_dummy = cosm.join(df_2).drop('skin_type', axis = 1)
+
+
+# ingredients
+pattern = "^-(\w|:|\s|,|.)+\r\n\r\n"
+pattern = "^-[:print:]+\r\n\r\n"
+
+a = re.sub(pattern, " ", a)
+b = pattern.search(a)
+b.group()
+
+b = []
+for i in range(5):
+    b = re.sub(pattern, "", cosm['ingredients'][i])
 
 
 ####################### 3. Exploratory Data Anaylsis
+# 1. Label -> item type
+
+
+
+
+# 2. rank distributions
+
+
+
+
+
+
+# 3. skin_type & counts, items, rank...
+
+
+
+
+
 
 
 
 
 
 ####################### 4. Clustering
+from nltk.tokenize import regexp_tokenize
 
+regexp_tokenize(', ')
 
+pattern = re.compile(r"^-(\w|:|\s|,|.)+\r\n\r\n")
+
+word_index_map = {}
+index_word_map = []
+current_index = 0
+corpus = []
+all_ingredients = []
+
+for i in range(len(cosm)):
+    text = re.sub(pattern, "", cosm['ingredients'][i])
+    text = text.lower()
+    tokens = regexp_tokenize(', ')
+    corpus.append(tokens)
+    for token in tokens:
+        if token not in word_index_map:
+            word_index_map[token] = current_index
+            current_index += 1
+            index_word_map.append(token)
+
+index_word_map[426]
+word_index_map['']
+
+def tokens_to_vector(tokens):
+    # initialize empty array
+    x = np.zeros(len(word_index_map))
+    # vectorize
+    for token in tokens:
+        i = word_index_map[token]
+        x[i] = 1
+    return x
+
+for tokens in corpus:
+    X[:, i] = tokens_to_vector(tokens)
+    i += 1
 
 
 
